a881a3c285989ca336ad96b09f047336409d69ab
Chen Zou <chenzou@uchicago.edu>
2022-01-11 14:23:05 -0600

    One IO trace for each core

--- python3/iotanalysis.py
+++ python3/iotanalysis.py
@@ -4,23 +4,20 @@ from absl import logging
 from collections import deque
 
 
-def iotanalysis(itp, otp, bias):
+def iotanalysis(itp, otp):
     delays = 0
     with open(itp, "r") as fi, open(otp, "r") as fo:
         issues = []
         ddls = []
         latencys = []
         line = fo.readline()
-        n = 0
         for line in fi:
             (issue, ddl, addr, _, _) = map(lambda x: int(x), line.split())
             line = fo.readline()
-            (_, _, latency) = map(lambda x: int(x), line.split())
-            n += 1
-            if addr < bias + bias:
-                issues.append(issue)
-                ddls.append(ddl)
-                latencys.append(latency)
+            (_, latency, _) = map(lambda x: int(x), line.split())
+            issues.append(issue)
+            ddls.append(ddl)
+            latencys.append(latency)
 
         queue = deque()
         d = 0
@@ -31,28 +28,27 @@ def iotanalysis(itp, otp, bias):
                     d += 1
                     delay = queue[0][0] + queue[0][2] - nano
                     delays += delay
-                    logging.info("%d %d %d %d %.3lf" %
-                                 (delay, queue[0][0], queue[0][1] -
-                                  queue[0][0], queue[0][2], float(delay) /
-                                  (queue[0][1] - queue[0][0])))
+                    logging.debug("d=%d issue=%d slack=%d lat=%d d/s=%.3lf" %
+                                  (delay, queue[0][0], queue[0][1] -
+                                   queue[0][0], queue[0][2], float(delay) /
+                                   (queue[0][1] - queue[0][0])))
                 queue.popleft()
                 nano = issues[i] + delays
 
             queue.append((issues[i] + delays, ddls[i] + delays, latencys[i]))
 
-    logging.info("%d out of %d analyzed, %d delayed" % (len(issues), n, d))
+    logging.info("%d analyzed, %d delayed" % (len(issues), d))
 
     return delays
 
 
 def main(argv):
     F = flags.FLAGS
-    print(iotanalysis(F.itp, F.otp, F.bias))
+    print(iotanalysis(F.itp, F.otp))
 
 
 if __name__ == "__main__":
-    flags.DEFINE_string("itp", "streambuffer.io", "input trace path")
+    flags.DEFINE_string("itp", "streambuffer.io.00", "input trace path")
     flags.DEFINE_string("otp", "cstoragetrace.IO_Flow.No_0.log",
                         "output trace path")
-    flags.DEFINE_integer("bias", (1 << 21) + 5, "Page address bias per core")
     app.run(main)
--- python3/packet2io.py
+++ python3/packet2io.py
@@ -6,7 +6,7 @@ from collections import deque
 from trace_stream import trace_stream
 
 
-def packet2io(pbpath, vague, reqnano, tsbsize, pagesize, offset, cores, bias):
+def packet2io(pbpath, vague, reqnano, tsbsize, pagesize, offset):
     (ticknano, istream, ostream) = trace_stream(pbpath, vague)
     ticknano //= int(1e9)
     ns = len(istream) + len(ostream)
@@ -34,60 +34,51 @@ def packet2io(pbpath, vague, reqnano, tsbsize, pagesize, offset, cores, bias):
         spp = pagesize >> 9
 
         if i < len(istream):
-            for j in range(pps):
+            for _ in range(pps):
                 p.append(0)
             for addr in sorted(d.keys()):
                 p.append(d[addr])
                 x = p.popleft()
                 y = c[addr]
-                iot.append(
-                    [x, y, addr * spp, spp, 1 if i < len(istream) else 0])
+                iot.append([x, y, addr * spp, spp, 1])
         else:
-            for j in range(pps):
+            for _ in range(pps):
                 p.append(lasttick * 2 // ticknano)
             for addr in sorted(d.keys(), reverse=True):
                 x = d[addr]
                 y = p.popleft()
                 p.append(c[addr])
-                iot.append(
-                    [x, y, addr * spp, spp, 1 if i < len(istream) else 0])
+                iot.append([x, y, addr * spp, spp, 0])
 
     iot = list(sorted(iot))
     for i in range(len(iot) - 1):
         iot[i + 1][0] = max(iot[i][0] + reqnano, iot[i + 1][0])
 
-    nano = reqnano << 3
-    aiot = []
-    for j in range(cores):
-        for i in range(len(iot)):
-            x = iot[i].copy()
-            x[0] += j * nano
-            x[1] += j * nano
-            x[2] += j * bias
-            aiot.append(x)
-
-    aiot = list(sorted(aiot))
-
-    return aiot
+    return iot
 
 
 def main(argv):
     F = flags.FLAGS
     iot = packet2io(F.pbpath, F.vague, F.reqnano, F.tsbsize, F.pagesize,
-                    F.offset, F.cores, F.bias)
-    for l in iot:
-        print(" ".join(map(lambda x: str(x), l)))
+                    F.offset)
+    for i in range(F.cores):
+        with open(F.output % i, "w") as fo:
+            for l in iot:
+                l[2] += F.bias
+                fo.write(" ".join(map(lambda x: str(x), l)))
+                fo.write("\n")
 
 
 if __name__ == "__main__":
     flags.DEFINE_string("pbpath", None, "path of the pb file")
-    flags.DEFINE_integer("vague", 1, "vaugeness when compositing stream")
+    flags.DEFINE_integer("vague", 16, "vaugeness when compositing stream")
     flags.DEFINE_integer("reqnano", 10, "request latency in ns")
     flags.DEFINE_integer("tsbsize", 131072, "total streambuffer size")
     flags.DEFINE_integer("pagesize", 4096, "page size")
     flags.DEFINE_integer("offset", 1 << 40, "Address offset in packets")
     flags.DEFINE_integer("cores", 8, "number of cores")
     flags.DEFINE_integer("bias", (1 << 21) + 11, "Page address bias per core")
+    flags.DEFINE_string("output", "streambuffer.io.%02d", "output re")
 
     flags.mark_flag_as_required("pbpath")
     app.run(main)
