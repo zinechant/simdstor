4ca1876c4428c82333e1e963e6f63188a60481ad
Chen Zou <chenzou@uchicago.edu>
2022-11-01 23:58:55 -0500

    tribus of eau

---
+++ eau/src/trimux.sv
@@ -0,0 +1,29 @@
+module trimux (
+    inum, ilen, ipos, psum, index
+);
+    parameter VLEN = 256, BSW = 5;
+    localparam BS = 1 << BSW;
+    localparam BLEN = VLEN / BS;
+    localparam WW = 8 - BSW + 1;
+
+    input wire [BSW : 0] inum;
+    input wire [WW - 1 : 0] ilen[BS];
+    input wire [BSW - 1 : 0] ipos[BS];
+    input wire [BSW - 1 : 0] psum[BS];
+    output tri [BSW : 0] index[BS];
+
+    logic [BSW - 1 : 0] prev[BS];
+
+    genvar i, j, k;
+    generate
+        for (k = 0; k < BS; k++) begin
+            assign prev[k] = (k == 0) ? '0 : psum[k - 1];
+        end
+        for (i = 0; i < BS; i++) begin
+            for (j = 0; j < BS; i++) begin
+                assign index[i] = (j >= inum || prev[j] > i || psum[j] <= i) ?
+                    'z : ((i - prev[j] < ilen[j]) ? ipos[j] + i - prev[j] : '1);
+            end
+        end
+    endgenerate
+endmodule
---
+++ eau/test/tb_trimux.sv
@@ -0,0 +1,36 @@
+module tb_trimux();
+
+parameter CP = 10;
+
+parameter VLEN = 256, BSW = 5;
+localparam BS = 1 << BSW;
+localparam BLEN = VLEN / BS;
+localparam WW = 8 - BSW + 1;
+
+
+logic [BSW : 0] inum;
+logic [WW - 1 : 0] ilen[BS];
+logic [BSW - 1 : 0] ipos[BS];
+logic [BSW - 1 : 0] psum[BS];
+logic [BSW : 0] index[BS];
+
+
+initial begin
+    inum = 5'd7;
+    psum = {4, 10, 13, 15, 19, 22, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    ilen = {4, 6, 3, 2, 2, 3, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    ipos = {5, 11, 16, 18, 21, 24, 29, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    #CP
+    #CP
+    #CP
+    #CP
+    ilen = {2, 3, 3, 2, 4, 3, 3, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    ipos = {2, 6, 9, 11, 15, 19, 22, 25, 29, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    #CP
+    #CP
+    #CP
+    #CP
+    inum = 5'd7;
+end
+
+endmodule
